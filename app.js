84e9f240cffe8f54f04b46f4d8673263268e62f8
alanluis <alan.luisva@gmail.com>
2022-08-25 11:55:18 -0500

    Create function getComputerChoice()
    You are going to play against the computer, getComputerChoice()  will
    randomly return either Rock, Paper or Scissors. We'll use this function
    in the game to make the computer's play.

---
+++ app.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1
+    if (computerChoice === 1) {
+        return "Rock"
+    } else if (computerChoice === 2) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
