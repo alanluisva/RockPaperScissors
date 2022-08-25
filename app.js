a152d131fd28f26b62103e9f0e3955018e200248
alanluis <alan.luisva@gmail.com>
2022-08-25 15:38:45 -0500

    Write a function that plays a single round of Rock Paper Scissors
    The function take two parameters: the playerSelection and
    computerSelection, and then return a string that declares the winner of
    the round.
    
    The parameters are case-insensitive (so users can input rock, ROCK,
    RocK or any other variation).

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3) + 1
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
     if (computerChoice === 1) {
-        return "Rock"
+        return "Rock";
     } else if (computerChoice === 2) {
-        return "Paper"
+        return "Paper";
     } else {
-        return "Scissors"
+        return "Scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    let player1 = playerSelection.toLowerCase();
+    let player2 = computerSelection.toLowerCase();
+
+    if (player1 === "rock") {
+        if (player2 === "scissors") {
+            return "You win! Rock beats Scissors";
+        } else if (player2 === "paper") {
+            return "You Lose! Paper beats Rock";
+        } else if (player2 === "rock") {
+            return "Draw!";
+        }
+    } else if (player1 === "scissors") {
+        if (player2 === "scissors") {
+            return "Draw";
+        } else if (player2 === "paper") {
+            return "You win! Scissors beats Paper";
+        } else if (player2 === "rock") {
+            return "You Lose! Rock beats  scissors";
+        }
+    } else if (player1 === "paper") {
+        if (player2 === "scissors") {
+            return "You Lose! Scissors beats Paper";
+        } else if (player2 === "paper") {
+            return "Draw!";
+        } else if (player2 === "rock") {
+            return "You Win! Paper beats  Rock";
+        }
     }
 }
