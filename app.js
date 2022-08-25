37c07ea1924c0e00cba30ef9e874fc3d0e097556
alanluis <alan.luisva@gmail.com>
2022-08-25 17:04:52 -0500

    Write a function called game()
    This function calls the playRound function to play a 5 round game
    that keeps score and reports a winner or loser at the end.
    
    Use prompt() to get input from the user.

--- app.js
+++ app.js
@@ -9,33 +9,61 @@ function getComputerChoice() {
     }
 }
 
+let player1Score = 0
+let player2Score = 0
+
 function playRound(playerSelection, computerSelection) {
     let player1 = playerSelection.toLowerCase();
     let player2 = computerSelection.toLowerCase();
 
     if (player1 === "rock") {
         if (player2 === "scissors") {
+            player1Score++
             return "You win! Rock beats Scissors";
         } else if (player2 === "paper") {
+            player2Score++
             return "You Lose! Paper beats Rock";
         } else if (player2 === "rock") {
             return "Draw!";
         }
     } else if (player1 === "scissors") {
         if (player2 === "scissors") {
-            return "Draw";
+            return "Draw!";
         } else if (player2 === "paper") {
+            player1Score++
             return "You win! Scissors beats Paper";
         } else if (player2 === "rock") {
-            return "You Lose! Rock beats  scissors";
+            player2Score++
+            return "You Lose! Rock beats scissors";
         }
     } else if (player1 === "paper") {
         if (player2 === "scissors") {
+            player2Score++
             return "You Lose! Scissors beats Paper";
         } else if (player2 === "paper") {
             return "Draw!";
         } else if (player2 === "rock") {
-            return "You Win! Paper beats  Rock";
+            player1Score++
+            return "You Win! Paper beats Rock";
         }
     }
 }
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Choose one of three shapes (rock, paper or scissors)");
+        const computerSelection = getComputerChoice();
+
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player score: ${player1Score} vs Computer score: ${player2Score}`)
+        console.log("-------------------------------------")
+    } if (player1Score > player2Score) {
+        console.log("YOU WON THE GAME!")
+    } else if (player1Score < player2Score) {
+        console.log("THE COMPUTER WON THE GAME!")
+    } else {
+        console.log("IT'S A TIE!")
+    }
+}
+
+game()
