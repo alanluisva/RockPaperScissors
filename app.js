675dbfe0c7b15fa153be43fe5d51357a7da09e3a
alanluis <alan.luisva@gmail.com>
2022-08-25 21:47:39 -0500

    Rewrite the playRound function to make more readable
    Instead of chaining if statements, the new function takes two
    parameters and then checks if computer's choice is the same as player's
    if not who wins.

--- app.js
+++ app.js
@@ -13,40 +13,21 @@ let player1Score = 0;
 let player2Score = 0;
 
 function playRound(playerSelection, computerSelection) {
-    let player1 = playerSelection.toLowerCase();
-    let player2 = computerSelection.toLowerCase();
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection.toLowerCase();
 
-    if (player1 === "rock") {
-        if (player2 === "scissors") {
-            player1Score++;
-            return "You win! Rock beats Scissors";
-        } else if (player2 === "paper") {
-            player2Score++;
-            return "You Lose! Paper beats Rock";
-        } else if (player2 === "rock") {
-            return "Draw!";
-        }
-    } else if (player1 === "scissors") {
-        if (player2 === "scissors") {
-            return "Draw!";
-        } else if (player2 === "paper") {
-            player1Score++;
-            return "You win! Scissors beats Paper";
-        } else if (player2 === "rock") {
-            player2Score++;
-            return "You Lose! Rock beats scissors";
-        }
-    } else if (player1 === "paper") {
-        if (player2 === "scissors") {
-            player2Score++;
-            return "You Lose! Scissors beats Paper";
-        } else if (player2 === "paper") {
-            return "Draw!";
-        } else if (player2 === "rock") {
-            player1Score++;
-            return "You Win! Paper beats Rock";
-        }
+    if (player === computer) {
+        return "Draw!";
+    } else if (
+        (player === "rock" && computer === "scissors") ||
+        (player === "scissors" && computer === "paper") ||
+        (player === "paper" && computer === "rock")
+    ) {
+        player1Score++;
+        return `You Win! ${player} beats ${computer}`;
     }
+    player2Score++;
+    return `You Lose! ${computer} beats ${player}`;
 }
 
 function game() {
@@ -57,8 +38,8 @@ function game() {
         const computerSelection = getComputerChoice();
 
         let result = playRound(playerSelection, computerSelection);
-        console.log(result);
         alert(result);
+        console.log(result);
         console.log(
             `Player score: ${player1Score} vs Computer score: ${player2Score}`
         );
